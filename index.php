b0090ebd97c015ed989373cbb8106f4c414e4969
Alex Roye <alex@creativefaze.com>
2017-07-11 15:43:34 +0500

    Customization to index page for date nd time

--- index.php
+++ index.php
@@ -7,38 +7,45 @@
   <script type="text/javascript" src="js/require.js"></script>
   <link rel="stylesheet" type="text/css" href="foxholder/src/foxholder-styles.css">
   <link rel="stylesheet" type="text/css" href="voice_input/speech-input.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
   <!-- <link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'> -->
   <script type="text/javascript" src="foxholder/src/foxholder.js"></script>
   <script type="text/javascript" src="voice_input/speech-input.js"></script>
   <script type="text/javascript">
     $(document).ready(function() {
+      function checkTime(i) {
+        if (i < 10) {
+          i = "0" + i;
+        }
+        return i;
+      }
+
+      function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        // add a zero in front of numbers<10
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function() {
+          startTime()
+        }, 500);
+      }
+      startTime();
       $('.form-container-1').foxholder({
         demo: 2 //or other number of demo (1-15) you want to use
       });
     });
   </script>
-  <style type="text/css">
-    @font-face {
-      font-family: 'Orbitron';
-      font-style: normal;
-      font-weight: 400;
-      src: local('Orbitron Regular'), local('Orbitron-Regular'), url(css/my-font.woff2) format('woff2');
-      unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
-    }
-    body {
-      background: #333333;
-      color: limegreen;
-      text-shadow: 0 0 5px limegreen;
-      font-family: 'Orbitron', sans-serif;
-    }
-  </style>
 </head>
 <body>
-  <?php print_r(rand(1,9)); ?>
-  <h1 class="color">Welcome</h1>
+  <h1>Welcome</h1>
   <div class="form-container-1">
-    <h3 class="text-center">Demo 1</h3>
-    <p class="text-muted text-center">Click on textarea or input to check</p>
+    <h3>Demo 1</h3>
+    <p>Click on textarea or input to check</p>
     <form>
       <input id="first-input-1" type="text" placeholder="First Input" required />
       <input id="second-input-1" type="email" placeholder="Second Input" required />
@@ -46,9 +53,20 @@
       <button type="submit">Submit</button>
     </form>
   </div>
-  <?php
-    $string = "1&du*m my.pdf";
-    $string = preg_replace('/[^a-zA-Z0-9_.]/', '_', $string);
-    print $string;
-  ?>
+  <hr>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <?php
+          $string = "1&du*m my.pdf";
+          $string = preg_replace('/[^a-zA-Z0-9_.]/', '_', $string);
+          print $string;
+        ?>
+      </div>
+      <div class="col-lg-6">
+        <h1 id="time"></h1>
+        <h2><?php print date('D, F d, Y', time()); ?></h2>
+      </div>
+    </div>
+  </div>
 </body>
---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Test</title>
+</head>
+<body>
+  <h1>Welcome</h1>
+  <h3>Demo 1</h3>
+  <p>There goes some text in paragraph, a part of this text can be <strong>strong</strong> or some can be a <a href="http://www.google.com" target="_blank">link</a></p>
+  <img src="ninja.gif" alt="This is picture of a ninja" style="box-shadow: 0 0 5px;">
+  <p>Add any image to the page as well.</p>
+  <p>This is some other text, some part of it are colorful <span style="color: red">like</span> <span style="color: green">this</span>. <span style="color: brown">We</span> <span style="color: grey">are</span> colorful.</p>
+</body>
+</html>
